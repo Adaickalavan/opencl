7eaa5af00f7bfebd11b5dd733fc62686fc7f54a4
adaickalavan <adaickalavan@gmail.com>
2019-12-11 20:03:44 +0800

    Update

--- apps/Ch7/callback.c
+++ apps/Ch7/callback.c
@@ -1,10 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define PROGRAM_FILE "callback.cl"
+#define NUM_FILES 1
+#define PROGRAM_FILE "./apps/Ch7/callback.cl"
 #define KERNEL_FUNC "callback"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "util.h"
 
 #ifdef MAC
 #include <OpenCL/cl.h>
@@ -36,84 +38,6 @@ void CL_CALLBACK read_complete(cl_event e, cl_int status, void* data) {
       printf("The data has not been initialized successfully.\n");
 }
 
-/* Find a GPU or CPU associated with the first available platform */
-cl_device_id create_device() {
-
-   cl_platform_id platform;
-   cl_device_id dev;
-   int err;
-
-   /* Identify a platform */
-   err = clGetPlatformIDs(1, &platform, NULL);
-   if(err < 0) {
-      perror("Couldn't identify a platform");
-      exit(1);
-   } 
-
-   /* Access a device */
-   err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &dev, NULL);
-   if(err == CL_DEVICE_NOT_FOUND) {
-      err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, 1, &dev, NULL);
-   }
-   if(err < 0) {
-      perror("Couldn't access any devices");
-      exit(1);   
-   }
-
-   return dev;
-}
-
-/* Create program from a file and compile it */
-cl_program build_program(cl_context ctx, cl_device_id dev, const char* filename) {
-
-   cl_program program;
-   FILE *program_handle;
-   char *program_buffer, *program_log;
-   size_t program_size, log_size;
-   int err;
-
-   /* Read program file and place content into buffer */
-   program_handle = fopen(filename, "r");
-   if(program_handle == NULL) {
-      perror("Couldn't find the program file");
-      exit(1);
-   }
-   fseek(program_handle, 0, SEEK_END);
-   program_size = ftell(program_handle);
-   rewind(program_handle);
-   program_buffer = (char*)malloc(program_size + 1);
-   program_buffer[program_size] = '\0';
-   fread(program_buffer, sizeof(char), program_size, program_handle);
-   fclose(program_handle);
-
-   /* Create program from file */
-   program = clCreateProgramWithSource(ctx, 1, 
-      (const char**)&program_buffer, &program_size, &err);
-   if(err < 0) {
-      perror("Couldn't create the program");
-      exit(1);
-   }
-   free(program_buffer);
-
-   /* Build program */
-   err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-   if(err < 0) {
-
-      /* Find size of log and print to std output */
-      clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, 
-            0, NULL, &log_size);
-      program_log = (char*) malloc(log_size + 1);
-      program_log[log_size] = '\0';
-      clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, 
-            log_size + 1, program_log, NULL);
-      printf("%s\n", program_log);
-      free(program_log);
-      exit(1);
-   }
-
-   return program;
-}
-
 int main() {
 
    /* OpenCL data structures */
@@ -121,6 +45,8 @@ int main() {
    cl_context context;
    cl_command_queue queue;
    cl_program program;
+   const char* fileNames[] = {PROGRAM_FILE};
+   const char options[] = "";     
    cl_kernel kernel;
    cl_int err;
 
@@ -139,7 +65,8 @@ int main() {
    }     
 
    /* Build the program and create a kernel */
-   program = build_program(context, device, PROGRAM_FILE);
+   // program = build_program(context, device, PROGRAM_FILE);
+   program = createProgramFromFile(NUM_FILES, fileNames, context, device, options);
    kernel = clCreateKernel(program, KERNEL_FUNC, &err);
    if(err < 0) {
       perror("Couldn't create a kernel");
@@ -162,14 +89,20 @@ int main() {
    };
 
    /* Create a command queue */
-   queue = clCreateCommandQueue(context, device, 0, &err);
+   queue = clCreateCommandQueueWithProperties(context, device, 0, &err);
    if(err < 0) {
       perror("Couldn't create a command queue");
       exit(1);   
    };
 
    /* Enqueue kernel */
-   err = clEnqueueTask(queue, kernel, 0, NULL, &kernel_event);
+   size_t dim = 1;
+   size_t global_size[1] = {1};
+   size_t* global_offset = NULL;
+   size_t* local_size = NULL;
+   err = clEnqueueNDRangeKernel(queue, kernel, dim, global_offset,
+         global_size, local_size, 0, NULL, &kernel_event);      
+   // err = clEnqueueTask(queue, kernel, 0, NULL, &kernel_event);
    if(err < 0) {
       perror("Couldn't enqueue the kernel");
       exit(1);   
