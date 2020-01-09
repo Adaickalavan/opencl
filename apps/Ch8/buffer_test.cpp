44c59cf9c6fb139780d2d3f9282a05e791d612ac
adaickalavan <adaickalavan@gmail.com>
2020-01-09 18:30:45 +0800

    Update

--- apps/Ch8/buffer_test.cpp
+++ apps/Ch8/buffer_test.cpp
@@ -1,7 +1,7 @@
 #define CL_HPP_ENABLE_EXCEPTIONS 
 #define CL_HPP_TARGET_OPENCL_VERSION 200
 // #define __NO_STD_VECTOR
-#define PROGRAM_FILE "./apps/Ch8/blank.cl"
+#define PROGRAM_FILE "./apps/Ch8/buffer_test.cl"
 #define KERNEL_FUNC "blank"
 
 #include <cstdio>
@@ -19,24 +19,24 @@ int main(void) {
    
    cl::vector<cl::Platform> platforms;
    cl::vector<cl::Device> devices;
-   int i, j;
 
    // Data and rectangle geometry
    float fullMatrix[80], zeroMatrix[80];
-   std::array<int,3> bufferOrigin; 
-   std::array<int,3> hostOrigin; 
-   std::array<int,3> region;
+   // cl::size_t<3> bufferOrigin, hostOrigin, region;
+   cl::array<cl::size_type, 3> bufferOrigin;
+   cl::array<cl::size_type, 3> hostOrigin;
+   cl::array<cl::size_type, 3> region;
 
    try {
       // Initialize data
-      for(i=0; i<80; i++) {
+      for(int i=0; i<80; i++) {
          fullMatrix[i] = i*1.0f;
          zeroMatrix[i] = 0.0f;
       }
 
       // Place the GPU devices of the first platform into a context
       cl::Platform::get(&platforms);
-      platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &devices);
+      platforms[0].getDevices(CL_DEVICE_TYPE_CPU, &devices);
       cl::Context context(devices);
       
       // Create kernel
@@ -54,35 +54,43 @@ int main(void) {
       // Create matrix buffer and make it the kernel's first argument
       cl::Buffer matrixBuffer(context, 
          CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, 
-         sizeof(zeroMatrix), zeroMatrix);
+         sizeof(zeroMatrix), zeroMatrix); 
       kernel.setArg(0, matrixBuffer);
 
       // Create queue and enqueue kernel-execution command
       cl::CommandQueue queue(context, devices[0]);
-      queue.enqueueTask(kernel);
+      // queue.enqueueTask(kernel);
+      cl::NDRange global_offset(0);
+      cl::NDRange global_size(1);
+      cl::NDRange local_size = cl::NullRange;
+      queue.enqueueNDRangeKernel(kernel, global_offset, 
+         global_size, local_size);
 
       // Update the content of the buffer
       queue.enqueueWriteBuffer(matrixBuffer, CL_TRUE, 0, 
             sizeof(fullMatrix), fullMatrix);
 
       // Read a rectangle of data from the buffer
-      bufferOrigin.push_back(5*sizeof(float)); 
-      bufferOrigin.push_back(3);
-      bufferOrigin.push_back(0);
-      hostOrigin.push_back(1*sizeof(float)); 
-      hostOrigin.push_back(1);
-      hostOrigin.push_back(0);
-      region.push_back(4*sizeof(float)); 
-      region.push_back(4);
-      region.push_back(1);
+      // bufferOrigin.push_back(5*sizeof(float)); 
+      // bufferOrigin.push_back(3);
+      // bufferOrigin.push_back(0);
+      bufferOrigin = {5*sizeof(float),3,0};
+      // hostOrigin.push_back(1*sizeof(float)); 
+      // hostOrigin.push_back(1);
+      // hostOrigin.push_back(0);
+      hostOrigin = {1*sizeof(float),1,0};
+      // region.push_back(4*sizeof(float)); 
+      // region.push_back(4);
+      // region.push_back(1);
+      region = {4*sizeof(float),4,1};
       queue.enqueueReadBufferRect(matrixBuffer, CL_TRUE, 
          bufferOrigin, hostOrigin, region, 
          10*sizeof(float), 0, 
          10*sizeof(float), 0, zeroMatrix);
 
       /* Display updated buffer */
-      for(i=0; i<8; i++) {
-         for(j=0; j<10; j++) {
+      for(int i=0; i<8; i++) {
+         for(int j=0; j<10; j++) {
             printf("%6.1f", zeroMatrix[j+i*10]);
          }
          printf("\n");
